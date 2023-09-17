3292ea7d33d10e6b615c3d09812ea723b51f26f6
dev-deeplake <127172647+dev-deeplake@users.noreply.github.com>
2023-09-17 21:13:17 +0900

    ThumbnailStyle.jsx ::: css to styled-components

--- src/styled-components/allsites/ThumbnailStyle.jsx
+++ src/styled-components/allsites/ThumbnailStyle.jsx
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 
 const ThumbnailStyle = styled.div`
-    width: ${props => props.theme.site_info_width};
+    width: var(--site-info-width);
     height: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
-    /* background-color: whitesmoke; */
+    background-color: whitesmoke;
 
     > img {
         width: 100%;
@@ -14,11 +14,16 @@ const ThumbnailStyle = styled.div`
         border-radius: 10px 10px 0 0;
         transition: transform 0.1s ease-in-out;
         cursor: pointer;
+
+        &:hover {
+            transform: scaleX(1.015) scaleY(1.015);
+            border-radius: 10px 10px 0px 0px;
+        }
     }
 
-    > .site-information {
+    /* > .site-information {
         
-    }
+    } */
 `
 
 export default ThumbnailStyle
